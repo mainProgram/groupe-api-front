8972a765bd8ee2570c57d83ad79ee71ef11b69a9
mainProgram <fazeyfadiop@gmail.com>
2023-01-17 17:47:37 +0000

    Route accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from "./public/not-found/not-found.component";
 const routes: Routes = [
+  { path: '', loadChildren: () =>  import('./home/home.module').then(m => m.HomeModule)},
   { path: 'home', loadChildren: () =>  import('./home/home.module').then(m => m.HomeModule)},
   { path: 'sujets', loadChildren: () =>  import('./sujet/sujet.module').then(m => m.SujetModule)},
   { path: 'etudiants', loadChildren: () =>  import('./etudiant/etudiant.module').then(m => m.EtudiantModule)},
